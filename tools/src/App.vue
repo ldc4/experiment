67488175330aef85a5a3528293026237b894277f
ldc4 <ldc4@vip.qq.com>
2019-10-13 15:41:30 +0800

    验证diff工具包的API

---
+++ tools/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Diff />
+  </div>
+</template>
+
+<script>
+import Diff from './components/Diff.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Diff
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
