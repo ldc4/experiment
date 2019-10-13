92e06d8d01640ddd990229e382db98a2bd0c0eae
ldc4 <ldc4@vip.qq.com>
2019-10-13 15:41:30 +0800

    验证diff工具包的API

---
+++ tools/README.md
@@ -0,0 +1,8 @@
+# 用于验证npm工具包API
+
+
+## diff
+
+https://github.com/kpdecker/jsdiff
+
+
---
+++ tools/src/components/Diff.vue
@@ -0,0 +1,138 @@
+<template>
+  <div class="diff">
+    <template v-for="(item, key) in output">
+      <div :key="`${key}-title`">
+        {{ key }}
+      </div>
+      <div :key="`${key}-output`">
+        {{ JSON.stringify(item, null, '  ') }}
+      </div>
+      <div :key="`${key}-display`">
+        <div v-html="display[key]" />
+        <hr />
+      </div>
+    </template>
+  </div>
+  
+</template>
+
+<script>
+var Diff = require('diff');
+
+export default {
+  name: 'Diff',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      output: {},
+      display: {}
+    }
+  },
+  created() {
+    let oldStr = ''
+    let newStr = ''
+    let oldObj = {}
+    let newObj = {}
+    let oldArr = []
+    let newArr = []
+    let output = null, display = null
+
+    // 对比字符
+    oldStr = 'beep boop'
+    newStr = 'beep boob blah'
+    output = Diff.diffChars(oldStr, newStr)
+    display = this.format(output)
+    this.output.chars = output
+    this.display.chars = display
+
+    // 对比词
+    oldStr = 'beep boop'
+    newStr = 'beep boob blah'
+    output = Diff.diffWords(oldStr, newStr)
+    display = this.format(output)
+    this.output.words = output
+    this.display.words = display
+
+    // 对比词 带空格
+    oldStr = 'beep, boop'
+    newStr = 'beep, boob blah'
+    output = Diff.diffWordsWithSpace(oldStr, newStr)
+    display = this.format(output)
+    this.output.wordsWithSpace = output
+    this.display.wordsWithSpace = display
+
+    // 对比行
+    oldStr = 'beep boop'
+    newStr = 'beep boob blah'
+    output = Diff.diffLines(oldStr, newStr)
+    display = this.format(output)
+    this.output.lines = output
+    this.display.lines = display
+
+    // 对比句子
+    oldStr = 'beep. boop'
+    newStr = 'beep. boob blah'
+    output = Diff.diffSentences(oldStr, newStr)
+    display = this.format(output)
+    this.output.sentences = output
+    this.display.sentences = display
+
+    // 对比对象
+    oldObj = { a: 1, b: 'weedust' }
+    newObj=  { c: 2, b: 'ldc4' }
+    output = Diff.diffJson(oldObj, newObj)
+    display = this.format(output)
+    this.output.json = output
+    this.display.json = display
+
+    // 对比数组
+    oldArr = ['aaa', 'bbb', 'ccc']
+    newArr = ['aaa', 'ccc', 'ddd']
+    output = Diff.diffArrays(oldArr, newArr)
+    display = this.format(output)
+    this.output.arr = output
+    this.display.arr = display
+
+
+    let beforeData = [{ label: 'aaa' }, { label: 'bbb' }, { label: 'ccc' }]
+    let afterData = [{ label: 'aaa' }, { label: 'ccc' }, { label: 'ddd' }]
+    let args = ['', this.getStringFromData(beforeData), this.getStringFromData(afterData), '', '']
+    output = Diff.createPatch(...args)
+    display = Diff.parsePatch(output)
+    this.output.createPatch = output
+    this.display.createPatch = display
+
+    // console.log(this.output)
+    // console.log(this.display)
+  },
+  methods: {
+    format(output) {
+      let display = ''
+      output.forEach((item) => {
+        if (item.added) {
+          display += '<ins>' + item.value + '</ins>'
+        } else if (item.removed) {
+          display += '<del>' + item.value + '</del>'
+        } else {
+          display += item.value
+        }
+      })
+      return display
+    },
+    getStringFromData(data){
+      let s = ''
+      for(let i=0; i<data.length ; i++){
+        s += data[i].label +'\n'
+      }
+      return s
+    },
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
