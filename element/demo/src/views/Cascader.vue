5a23d62bf7899a5d8720b56d173ec8ca351c5482
ldc4 <ldc4@vip.qq.com>
2019-10-25 16:19:41 +0800

    验证cascader的value为空的情况

---
+++ element/demo/src/views/Cascader.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <el-cascader
+      :options="options"
+      :props="{ multiple: true, checkStrictly: true, emitPath: false }"
+      :value="value"
+      @change="change"
+      :show-all-levels="false"
+      filterable
+      clearable
+    ></el-cascader>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        value: [],
+        options: [
+          {
+            value: 'test1',
+            label: 'test1',
+          },
+          {
+            value: 'test2',
+            label: 'test2',
+          }
+        ]
+      };
+    },
+    methods: {
+      change(ids) {
+        this.value = ids
+        console.log(ids)
+      }
+    }
+  };
+</script>
